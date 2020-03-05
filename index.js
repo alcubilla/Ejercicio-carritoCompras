7470e0a336309dfc5d52c04f62c606f3f5389b19
Marina <alcubilla.marina@gmail.com>
2020-03-05 12:27:55 -0600

    sub apps

---
+++ index.js
@@ -0,0 +1,29 @@
+import http from 'http';
+import express from 'express';
+import productsList from './online_store/products'
+import productsRoutes from './online_store/productsRoutes'
+import adminRoutes from './online_store/adminRoutes'
+
+
+const APP = express();
+APP.use(express.json()); //para usar el body
+const PRODUCTS = express();
+const ADMIN = express();
+
+let total = 0;
+
+APP.use('/products', PRODUCTS);
+APP.use('/admin', ADMIN);
+
+const SERVER = http.createServer(APP);
+
+APP.get('/', (req,res) => {
+    res.send('API HOME'); 
+});
+
+
+total = productsRoutes(PRODUCTS, productsList, total);
+
+adminRoutes(ADMIN, productsList,total);
+
+SERVER.listen(5000);
